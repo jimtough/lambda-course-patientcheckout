21b8515237baec5a2a210e3eac8f8d292d3e43c3
adultmalehuman <adultmalehuman@gmail.com>
2021-10-22 13:15:31 -0300

    Implement BillManagementLambda

--- patientcheckout/src/main/java/org/jimtough/aws/lambda/s3sns/BillManagementLambda.java
+++ patientcheckout/src/main/java/org/jimtough/aws/lambda/s3sns/BillManagementLambda.java
@@ -1,9 +1,25 @@
 package org.jimtough.aws.lambda.s3sns;
 
+import com.amazonaws.services.lambda.runtime.events.SNSEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class BillManagementLambda {
 
-	public void handler() {
-		
+	private final ObjectMapper objectMapper = new ObjectMapper();
+	
+	public void handler(SNSEvent event) {
+		event.getRecords().forEach(snsRecord->{
+			try {
+				PatientCheckoutEvent patientCheckoutEvent =
+						objectMapper.readValue(
+							snsRecord.getSNS().getMessage(),
+							PatientCheckoutEvent.class);
+				System.out.println(patientCheckoutEvent);
+			} catch (JsonProcessingException e) {
+				e.printStackTrace();
+			}
+		});
 	}
 
 }
