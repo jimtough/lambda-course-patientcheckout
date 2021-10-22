c65aacf3d55522aeaebd850e7818520d3b425196
adultmalehuman <adultmalehuman@gmail.com>
2021-10-22 13:35:05 -0300

    Code refactoring

--- patientcheckout/src/main/java/org/jimtough/aws/lambda/s3sns/PatientCheckoutLambda.java
+++ patientcheckout/src/main/java/org/jimtough/aws/lambda/s3sns/PatientCheckoutLambda.java
@@ -15,30 +15,36 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class PatientCheckoutLambda {
 
+	private static final String PATIENT_CHECKOUT_TOPIC = System.getenv("PATIENT_CHECKOUT_TOPIC");
 	private final AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
 	private final AmazonSNS sns = AmazonSNSClientBuilder.defaultClient();
 	private final ObjectMapper objectMapper = new ObjectMapper();
 
 	public void handler(S3Event event) {
 		event.getRecords().forEach(record->{
-			S3ObjectInputStream inputStream = s3
-					.getObject(record.getS3().getBucket().getName(), record.getS3().getObject().getKey())
-					.getObjectContent();
-			try {
+			try (
+				S3ObjectInputStream inputStream = s3
+						.getObject(record.getS3().getBucket().getName(), record.getS3().getObject().getKey())
+						.getObjectContent()
+			) {
 				List<PatientCheckoutEvent> patientCheckoutEvents =
 						Arrays.asList(objectMapper.readValue(inputStream, PatientCheckoutEvent[].class));
 				// NOTE: System.out output will be logged in Cloudwatch Logs
 				System.out.println(patientCheckoutEvents);
-				patientCheckoutEvents.forEach(checkoutEvent->{
-					try {
-						sns.publish(
-							System.getenv("PATIENT_CHECKOUT_TOPIC"),
-							objectMapper.writeValueAsString(checkoutEvent));
-					} catch (JsonProcessingException e) {
-						e.printStackTrace();
-					}
-				});
+				publishMessageToSNS(patientCheckoutEvents);
 			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
+	}
+
+	private void publishMessageToSNS(final List<PatientCheckoutEvent> patientCheckoutEvents) {
+		patientCheckoutEvents.forEach(checkoutEvent->{
+			try {
+				sns.publish(
+					PATIENT_CHECKOUT_TOPIC,
+					objectMapper.writeValueAsString(checkoutEvent));
+			} catch (JsonProcessingException e) {
 				e.printStackTrace();
 			}
 		});
