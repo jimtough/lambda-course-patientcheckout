0610853f4f8794133724c25b6bd9c05e30912edb
adultmalehuman <adultmalehuman@gmail.com>
2021-10-22 12:30:23 -0300

    Implement PatientCheckoutLambda

--- patientcheckout/src/main/java/org/jimtough/aws/lambda/s3sns/PatientCheckoutLambda.java
+++ patientcheckout/src/main/java/org/jimtough/aws/lambda/s3sns/PatientCheckoutLambda.java
@@ -1,9 +1,34 @@
 package org.jimtough.aws.lambda.s3sns;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import com.amazonaws.services.lambda.runtime.events.S3Event;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class PatientCheckoutLambda {
 
-	public void handler() {
+	private final AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
+	private final ObjectMapper objectMapper = new ObjectMapper();
 
+	public void handler(S3Event event) {
+		event.getRecords().forEach(record->{
+			S3ObjectInputStream inputStream = s3
+					.getObject(record.getS3().getBucket().getName(), record.getS3().getObject().getKey())
+					.getObjectContent();
+			try {
+				List<PatientCheckoutEvent> patientCheckoutEvents =
+						Arrays.asList(objectMapper.readValue(inputStream, PatientCheckoutEvent[].class));
+				// NOTE: System.out output will be logged in Cloudwatch Logs
+				System.out.println(patientCheckoutEvents);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
 	}
 
 }
