d9d09abe5268d0583e773011a92a6c49494ffc4c
adultmalehuman <adultmalehuman@gmail.com>
2021-10-22 14:21:30 -0300

    Using Lamdba-supported log4j2 / slf4j logging (best option!)

--- patientcheckout/src/main/java/org/jimtough/aws/lambda/s3sns/PatientCheckoutLambda.java
+++ patientcheckout/src/main/java/org/jimtough/aws/lambda/s3sns/PatientCheckoutLambda.java
@@ -1,13 +1,10 @@
 package org.jimtough.aws.lambda.s3sns;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.util.Arrays;
 import java.util.List;
 
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
@@ -16,6 +13,8 @@ import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PatientCheckoutLambda {
 
@@ -25,23 +24,23 @@ public class PatientCheckoutLambda {
 	private final ObjectMapper objectMapper = new ObjectMapper();
 
 	public void handler(S3Event event, Context context) {
-		final LambdaLogger logger = context.getLogger();
+		//final LambdaLogger logger = context.getLogger();
+		Logger logger = LoggerFactory.getLogger(PatientCheckoutLambda.class);
+
 		event.getRecords().forEach(record->{
 			try (
 				S3ObjectInputStream inputStream = s3
 						.getObject(record.getS3().getBucket().getName(), record.getS3().getObject().getKey())
 						.getObjectContent()
 			) {
-				logger.log("Reading data from S3");
+				logger.info("Reading data from S3");
 				List<PatientCheckoutEvent> patientCheckoutEvents =
 						Arrays.asList(objectMapper.readValue(inputStream, PatientCheckoutEvent[].class));
-				logger.log(patientCheckoutEvents.toString());
-				logger.log("Publishing message to SNS");
+				logger.info(patientCheckoutEvents.toString());
+				logger.info("Publishing message to SNS");
 				publishMessageToSNS(patientCheckoutEvents);
 			} catch (IOException e) {
-				StringWriter stringWriter = new StringWriter();
-				e.printStackTrace(new PrintWriter(stringWriter));
-				logger.log(stringWriter.toString());
+				logger.error("exception caught", e);
 			}
 		});
 	}
