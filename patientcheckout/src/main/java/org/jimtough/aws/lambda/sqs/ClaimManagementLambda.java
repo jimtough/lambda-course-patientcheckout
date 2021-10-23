02888e564b60c798fcf964510f53be27378ba27e
adultmalehuman <adultmalehuman@gmail.com>
2021-10-23 11:33:24 -0300

    Create lambda that consumes SQSEvent

---
+++ patientcheckout/src/main/java/org/jimtough/aws/lambda/sqs/ClaimManagementLambda.java
@@ -0,0 +1,12 @@
+package org.jimtough.aws.lambda.sqs;
+
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+
+public class ClaimManagementLambda {
+
+	public void handler(SQSEvent event) {
+		event.getRecords().forEach(message -> {
+			System.out.println(message.getBody());
+		});
+	}
+}
